08aba0f33c6976657382b79a31cc03e3128e69f0
Will Forcey <wsforc3y@gmail.com>
2023-12-18 16:11:17 -0500

    Add files via upload

--- drawText.go
+++ drawText.go
@@ -7,8 +7,8 @@ import (
 	"strings"
 
 	"golang.org/x/image/font"
-	"golang.org/x/image/math/fixed"
 	"golang.org/x/image/font/basicfont"
+	"golang.org/x/image/math/fixed"
 
 	"golang.org/x/mobile/event/size"
 	"golang.org/x/mobile/exp/gl/glutil"
@@ -16,30 +16,30 @@ import (
 )
 
 func drawText(img *image.RGBA, x, y int, s string) {
-    col := color.RGBA{0, 0, 0, 255}
-    point := fixed.Point26_6{fixed.I(x), fixed.I(y)}
+	col := color.RGBA{0, 0, 0, 255}
+	point := fixed.Point26_6{fixed.I(x), fixed.I(y)}
 
-    d := &font.Drawer {
-        Dst:  img,
-        Src:  image.NewUniform(col),
-        Face: basicfont.Face7x13,
-        Dot:  point,
-    }
-    d.DrawString(s)
+	d := &font.Drawer{
+		Dst:  img,
+		Src:  image.NewUniform(col),
+		Face: basicfont.Face7x13,
+		Dot:  point,
+	}
+	d.DrawString(s)
 }
 
 // Text draws text to screen
 type Text struct {
-	sz       size.Event
-	images   *glutil.Images
-	m        *glutil.Image
+	sz     size.Event
+	images *glutil.Images
+	m      *glutil.Image
 	// TODO: store *gl.Context
 }
 
 // NewText creates a Text tied to the current GL context.
 func NewText(images *glutil.Images) *Text {
 	return &Text{
-		images:   images,
+		images: images,
 	}
 }
 
@@ -48,7 +48,7 @@ func NewText(images *glutil.Images) *Text {
 func (t *Text) Draw(sz size.Event, x, y int, scaleX, scaleY geom.Pt, s string) {
 	if sz.WidthPx == 0 && sz.HeightPx == 0 {
 		return
-	} else if t.m == nil {
+	} else if t.sz != sz {
 		t.sz = sz
 		if t.m != nil {
 			t.m.Release()
@@ -61,17 +61,17 @@ func (t *Text) Draw(sz size.Event, x, y int, scaleX, scaleY geom.Pt, s string) {
 
 	// draw each string on a seperate line
 	for i, v := range lines {
-		drawText(t.m.RGBA, int(geom.Pt(x) / scaleX), int(geom.Pt(y) / scaleY) + i * 10, v)
+		drawText(t.m.RGBA, int(geom.Pt(x)/scaleX), int(geom.Pt(y)/scaleY)+i*10, v)
 	}
-	
+
 	// copy img data to GL device
 	t.m.Upload()
 
 	t.m.Draw(
 		sz,
-		geom.Point{0, 0},	// topLeft	
-		geom.Point{sz.WidthPt * scaleX, 0},	// topRight
-		geom.Point{0, sz.HeightPt * scaleY},	// bottomLeft
+		geom.Point{0, 0},                    // topLeft
+		geom.Point{sz.WidthPt * scaleX, 0},  // topRight
+		geom.Point{0, sz.HeightPt * scaleY}, // bottomLeft
 		t.m.RGBA.Bounds(),
 	)
 }
